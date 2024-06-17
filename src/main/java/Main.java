9e2334640746c0f20a8e8859957693c6ab41f1d0
pedro-ribeiro-rci <pedro.ribeiro@rci.rogers.ca>
2024-06-17 20:13:19 +0100

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,13 +45,39 @@ public class Main {
       try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
           PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
-        String line;
-        while ((line = in.readLine()) != null) {
-          if (line.equalsIgnoreCase("ping")) {
-            out.println("+PONG\r");
+        while (true) {
+          String messageType = in.readLine();
+
+          if (messageType == null) {
+            break;
+          }
+
+          /*
+            * The Redis client sends a message in the following format:
+            * *<number of arguments>\r\n
+            * $<length of argument>\r\n
+            * <argument>\r\n
+            * $<length of argument>\r\n
+            * <argument>\r\n
+           */
+          if (messageType.startsWith("*")) {
+            int numArgs = Integer.parseInt(messageType.substring(1));
+            String[] args = new String[numArgs];
+            for (int i = 0; i < numArgs; i++) {
+              int argLength = Integer.parseInt(in.readLine().substring(1));
+              char[] argChars = new char[argLength];
+              in.read(argChars, 0, argLength);
+              in.readLine();
+              args[i] = new String(argChars);
+            }
+
+            String command = args[0].toUpperCase();
+            switch (command) {
+              case "PING" -> out.println(createStringResponse("PONG"));
+              case "ECHO" -> out.println(createStringResponse(args[1]));
+            }
           }
         }
-
       } catch (IOException e) {
         System.err.println("IOException handling client: " + e.getMessage());
       } finally {
@@ -62,6 +88,10 @@ public class Main {
           System.err.println("IOException closing client socket: " + e.getMessage());
         }
       }
+    }
+
+    private String createStringResponse(String response) {
+      return "$" + response.length() + "\r\n" + response + "\r";
     }
   }
 }
